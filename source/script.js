c6d6b0bb76dfafca7c0024580b3e3483f869ebd7
zidan <zidandewangga252@gmail.com>
2026-02-10 20:39:47 +0700

    nambah input

--- source/script.js
+++ source/script.js
@@ -6,16 +6,17 @@ let button = document.getElementById("tombol")
 button.addEventListener("click",function () {
     const Nama = document.getElementById("nama").value
     const Absen = document.getElementById("absen").value
+    const Kelas = document.getElementById("kelas").value
 
-    if (!Nama || !Absen) {
+    if (!Nama || !Absen || !Kelas) {
         alert(" Nama dan Absen wajib di isi ")
         return;
     } 
 
-     if (Nama === "a" && Absen === "1") {
+     if (Nama === "a" && Absen === "1" && Kelas ==="11") {
         alert("Login Berhasil")
 
      }else{
-        alert("NAMA ATAU ABSEN SALAH")
+        alert("AKUN TIDAK DI TEMUKAN")
      }
 })
