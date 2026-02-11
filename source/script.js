02fd365e3b1c3813d17b47c23aa5fe8c1e267043
zidan <zidandewangga252@gmail.com>
2026-02-11 08:22:55 +0700

    nambah logo

--- source/script.js
+++ source/script.js
@@ -6,14 +6,14 @@ let button = document.getElementById("tombol")
 button.addEventListener("click",function () {
     const Nama = document.getElementById("nama").value
     const Absen = document.getElementById("absen").value
-    const Kelas = document.getElementById("kelas").value
+    const Password = document.getElementById("password").value
 
-    if (!Nama || !Absen || !Kelas) {
+    if (!Nama || !Absen || !Password) {
         alert(" Nama dan Absen wajib di isi ")
         return;
     } 
 
-     if (Nama === "a" && Absen === "1" && Kelas ==="11") {
+     if (Nama === "a" && Absen === "1" && Password ==="11") {
         alert("Login Berhasil")
 
      }else{
